90667c43f75e4c2632642c9a027a3b088620ebcc
eric2054 <90565230+eric2054@users.noreply.github.com>
2024-05-14 15:19:48 +0800

    Update CardGame.h

--- CardGame.h
+++ CardGame.h
@@ -1,1 +1,23 @@
+#include <algorithm>
+#include <cstdlib>
+#include <cstdio>
+#include <ctime>
+#include <cmath>
+#include <string>
+#include <vector>
 
+using namespace std;
+
+class CardGame {
+public:
+    CardGame();
+    void startGame();
+
+private:
+    int Card[53], Card_InUsed[53], Card_Index, Desk[53], user_input;
+    int count, count2, rand_number, player_number;
+    bool Check_RanCardOK = true, Desk_Check = true;
+
+    string decode(int number);
+};
+#endif
