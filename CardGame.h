f7d30142bd6a78ac2438a0ca9d0d629b6d5e1187
eric2054 <90565230+eric2054@users.noreply.github.com>
2024-06-18 01:47:14 +0800

    Update CardGame.h

--- CardGame.h
+++ CardGame.h
@@ -10,14 +10,14 @@ using namespace std;
 
 class CardGame {
 public:
-    CardGame();
-    void startGame();
+    CardGame(); // 建構子
+    void startGame(); // 開始遊戲的函數
 
 private:
-    int Card[53], Card_InUsed[53], Card_Index, Desk[53], user_input;
-    int count, count2, rand_number, player_number;
-    bool Check_RanCardOK = true, Desk_Check = true;
+    int Card[53], Card_InUsed[53], Card_Index, Desk[53], user_input; // 卡片相關陣列和變數
+    int count, count2, rand_number, player_number; // 計數器和玩家號碼
+    bool Check_RanCardOK = true, Desk_Check = true; // 檢查卡片和桌面狀態的布林值
 
-    string decode(int number);
+    string decode(int number); // 將卡片編號解碼為字串表示的函數
 };
 #endif
